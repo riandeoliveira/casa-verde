21629602e7fd24c51f84439517aab4153ab2666b
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-13 01:06:20 -0300

    ✨ feat: Create App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { Header } from "./components/Header";
+import plants from "./content/plants.json";
+import { PlantCard } from "./components/PlantCard";
+import { plantImages } from "./assets";
+
 export const App = (): JSX.Element => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          {plants.map((plant, i) => (
+            <PlantCard
+              name={plant.name}
+              price={plant.price}
+              image={plantImages[i]}
+              key={i}
+            />
+          ))}
+        </section>
+      </main>
+    </>
+  );
 };
