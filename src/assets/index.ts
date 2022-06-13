1c75e27181cb81d00a7fcd3f606775ede05413c5
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-13 01:08:40 -0300

    ✨ feat: Add more images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,20 @@
-export {};
+import arrow_right from "./icons/arrow_right.svg";
+import bg_top from "./icons/bg_top.svg";
+import logo from "./icons/logo.svg";
+import ajuga_reptans from "./images/ajuga_reptans.png";
+import cordyline_fruticosa from "./images/cordyline_fruticosa.png";
+import crassula_ovata from "./images/crassula_ovata.png";
+import cyperus_rotundus from "./images/cyperus_rotundus.png";
+import datura_metel from "./images/datura_metel.png";
+import delairea_odorata from "./images/delairea_odorata.png";
+
+const plantImages: string[] = [
+  ajuga_reptans,
+  cordyline_fruticosa,
+  crassula_ovata,
+  cyperus_rotundus,
+  datura_metel,
+  delairea_odorata,
+];
+
+export { arrow_right, bg_top, logo, plantImages };
