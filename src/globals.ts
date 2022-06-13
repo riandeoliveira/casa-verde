79a0a4cab3051e661df3a780fa06c7e5e4af0f18
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-13 01:07:17 -0300

    ✨ feat: Add page background

--- src/globals.ts
+++ src/globals.ts
@@ -1,14 +1,25 @@
 import { createGlobalStyle } from "styled-components";
+import fonts from "./themes/fonts";
+import colors from "./themes/colors";
+import { bg_top } from "./assets";
 
 export const GlobalStyle = createGlobalStyle`  
   * {
     border: 0;
     box-sizing: border-box;
-    font-family: 'Roboto', sans-serif;
+    font-family: ${fonts.secondary}, ${fonts.default};
     list-style: none;
     margin: 0;
     outline: 0;
     padding: 0;
     text-decoration: none;
+  }
+
+  body {
+    background-attachment: fixed;
+    background-color: ${colors.neutral};
+    background-image: url(${bg_top});
+    background-position: top right;
+    background-repeat: no-repeat;
   }
 `;
