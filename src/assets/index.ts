14745085b07d567d126119bb865294adbfa07caf
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-14 16:15:57 -0300

    ✨ feat: Add new images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,14 +1,15 @@
-import arrow_right from "./icons/arrow_right.svg";
-import bg_top from "./icons/bg_top.svg";
-import ellipse from "./icons/ellipse.svg";
-import logo from "./icons/logo.svg";
-
 import ajuga_reptans from "./images/ajuga_reptans.png";
+import arrow_right from "./icons/arrow_right.svg";
+import best_plant from "./images/best_plant.png";
+import bg_top from "./icons/bg_top.svg";
 import cordyline_fruticosa from "./images/cordyline_fruticosa.png";
 import crassula_ovata from "./images/crassula_ovata.png";
 import cyperus_rotundus from "./images/cyperus_rotundus.png";
 import datura_metel from "./images/datura_metel.png";
 import delairea_odorata from "./images/delairea_odorata.png";
+import ellipse from "./icons/ellipse.svg";
+import logo from "./icons/logo.svg";
+import mail from "./icons/mail.svg";
 import my_plant from "./images/my_plant.png";
 
 const plantImages: string[] = [
@@ -16,8 +17,17 @@ const plantImages: string[] = [
   cordyline_fruticosa,
   crassula_ovata,
   cyperus_rotundus,
-  delairea_odorata,
   datura_metel,
+  delairea_odorata,
 ];
 
-export { arrow_right, bg_top, ellipse, logo, plantImages, my_plant };
+export {
+  arrow_right,
+  best_plant,
+  bg_top,
+  ellipse,
+  logo,
+  mail,
+  my_plant,
+  plantImages,
+};
