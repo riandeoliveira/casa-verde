d3668c481a220b09b9bccbfc601ef13e636290a7
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-06-14 14:18:28 -0300

    📦 refactor: Update PlantCard component

--- src/components/PlantCard/index.tsx
+++ src/components/PlantCard/index.tsx
@@ -8,21 +8,21 @@ interface PlantCardProps {
   price: number;
 }
 
-export const PlantCard = (props: PlantCardProps): JSX.Element => {
-  const { image, name, price } = props;
-
-  return (
-    <S.Card bgImage={image}>
-      <S.Container>
-        <S.NamePriceContainer>
-          <S.Name>{name}</S.Name>
-          <S.Price>R$ {price.toFixed(2).replace(".", ",")}</S.Price>
-        </S.NamePriceContainer>
-        <S.Button type="button">
-          Comprar
-          <img src={arrow_right} alt="Arrow right icon" />
-        </S.Button>
-      </S.Container>
-    </S.Card>
-  );
-};
+export const PlantCard = ({
+  image,
+  name,
+  price,
+}: PlantCardProps): JSX.Element => (
+  <S.Card bgImage={image}>
+    <div>
+      <div>
+        <h3>{name}</h3>
+        <span>R$ {price.toFixed(2).replace(".", ",")}</span>
+      </div>
+      <button type="button">
+        Comprar
+        <img src={arrow_right} alt="Arrow right icon" />
+      </button>
+    </div>
+  </S.Card>
+);
--- src/components/PlantCard/styles.ts
+++ src/components/PlantCard/styles.ts
@@ -17,40 +17,40 @@ export const Card = styled.div<CardProps>`
   flex-direction: column;
   height: 200px;
   width: 380px;
-`;
 
-export const NamePriceContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    height: inherit;
+    justify-content: space-between;
+    margin: 24px 24px 24px 0;
+    width: 160px;
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: inherit;
-  justify-content: space-between;
-  margin: 24px 24px 24px 0;
-  width: 160px;
-`;
+    div {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
 
-export const Name = styled.h3`
-  font-family: ${fonts.primary};
-  font-size: 32px;
-`;
+      h3 {
+        font-family: ${fonts.primary};
+        font-size: 32px;
+      }
 
-export const Price = styled.span`
-  font-weight: 600;
-  opacity: 0.5;
-`;
+      span {
+        font-weight: 600;
+        opacity: 0.5;
+      }
+    }
 
-export const Button = styled.button`
-  align-items: center;
-  background-color: transparent;
-  color: ${colors.secondary};
-  cursor: pointer;
-  display: flex;
-  font-size: 16px;
-  font-weight: bold;
-  gap: 30px;
+    button {
+      align-items: center;
+      background-color: transparent;
+      color: ${colors.secondary};
+      cursor: pointer;
+      display: flex;
+      font-size: 16px;
+      font-weight: bold;
+      gap: 30px;
+    }
+  }
 `;
