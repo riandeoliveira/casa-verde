389500e6ac345bc82d258119705b16d5aa43848f
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-16 16:49:19 -0300

    refactor: move nav links to a separate file

---
+++ src/content/nav_links.ts
@@ -0,0 +1,29 @@
+interface INavLinks {
+  title: string;
+  path: string;
+}
+
+const navLinks: INavLinks[] = [
+  {
+    title: "Como fazer",
+    path: "/como-fazer",
+  },
+  {
+    title: "Ofertas",
+    path: "/ofertas",
+  },
+  {
+    title: "Depoimentos",
+    path: "/depoimentos",
+  },
+  {
+    title: "Vídeos",
+    path: "/videos",
+  },
+  {
+    title: "Meu carrinho",
+    path: "/meu-carrinho",
+  },
+];
+
+export default navLinks;
