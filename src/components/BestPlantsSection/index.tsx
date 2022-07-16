55e830f6a38b135b401e2b699eed6fc5ba8ec002
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-16 16:51:17 -0300

    refactor: rebuild BestPlantsSection

--- src/components/BestPlantsSection/index.tsx
+++ src/components/BestPlantsSection/index.tsx
@@ -1,25 +1,51 @@
-import * as S from "./styles";
-import { mail } from "../../assets";
+import { TitleBox } from "components";
+import Image from "next/image";
+import styles from "./styles.module.scss";
 
-export const BestPlantsSection = (): JSX.Element => (
-  <S.Section>
-    <div>
-      <div>
-        <S.TitleContainer>
-          <h1>melhores plantas</h1>
-          <h2>Sua casa com as</h2>
-        </S.TitleContainer>
-        <p>
-          Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
-          torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail
-          e assine nossa newsletter para saber das novidades da marca.
-        </p>
-        <S.InputContainer>
-          <img src={mail} alt="Email icon" />
-          <input type="email" placeholder="Insira seu e-mail" />
-          <button type="button">Assinar newsletter</button>
-        </S.InputContainer>
+export const BestPlantsSection = (): JSX.Element => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.section_container}>
+        <div className={styles.container}>
+          <TitleBox subtitle="Sua casa com as" title="melhores plantas" />
+          <p className={styles.description}>
+            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+            torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu
+            e-mail e assine nossa newsletter para saber das novidades da marca.
+          </p>
+          <form className={styles.form}>
+            <div className={styles.input_container}>
+              <div className={styles.icon}>
+                <Image
+                  src="/assets/icons/email.svg"
+                  alt="Ícone de e-mail"
+                  width={22}
+                  height={10}
+                />
+              </div>
+              <input
+                type="email"
+                placeholder="Insira seu e-mail"
+                className={styles.input}
+                required
+              />
+            </div>
+            <button type="submit" className={styles.button}>
+              Assinar newsletter
+            </button>
+          </form>
+        </div>
+        <div className={styles.image}>
+          <Image
+            src="/assets/images/abc.svg"
+            alt="Imagem uma planta em um vaso"
+            width={580}
+            height={890}
+          />
+        </div>
       </div>
-    </div>
-  </S.Section>
-);
+    </section>
+  );
+};
+
+export default BestPlantsSection;
