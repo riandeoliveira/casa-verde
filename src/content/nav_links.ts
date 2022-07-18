f86962e5bcb50ed72b49e60fed43c3b595798272
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-18 01:14:32 -0300

    refactor: update nav_links.ts

--- src/content/nav_links.ts
+++ src/content/nav_links.ts
@@ -1,26 +1,28 @@
-interface INavLinks {
-  title: string;
-  path: string;
-}
+import type { INavLink } from "interfaces";
 
-const navLinks: INavLinks[] = [
+const navLinks: INavLink[] = [
   {
+    id: 1,
     title: "Como fazer",
     path: "/como-fazer",
   },
   {
+    id: 2,
     title: "Ofertas",
     path: "/ofertas",
   },
   {
+    id: 3,
     title: "Depoimentos",
     path: "/depoimentos",
   },
   {
+    id: 4,
     title: "Vídeos",
     path: "/videos",
   },
   {
+    id: 5,
     title: "Meu carrinho",
     path: "/meu-carrinho",
   },
