f7cb46b2105048433b7b1fd5159af4bd28c486da
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-18 22:54:52 -0300

    refactor: create DesktopNavbar component

--- src/components/DesktopNavbar/index.tsx
+++ src/components/DesktopNavbar/index.tsx
@@ -1,19 +1,12 @@
+import { NavLink } from "components";
 import { navLinks } from "content";
-import Link from "next/link";
 import styles from "./styles.module.scss";
 
 const DesktopNavbar = (): JSX.Element => {
   return (
     <ul className={styles.list}>
-      {navLinks.map((link, i) => (
-        <>
-          <li>
-            <Link href={link.path}>
-              <a className={styles.link}>{link.title}</a>
-            </Link>
-          </li>
-          {navLinks.length - 1 !== i && <li>/</li>}
-        </>
+      {navLinks.map(({ id, title, path }) => (
+        <NavLink id={id} title={title} path={path} key={id} />
       ))}
     </ul>
   );
